4c5e3586c06f8c2e4dc84a7d30fc9e4799c0ffbc
Daniel Le Cornu <dan@noquarter.co>
2024-10-28 00:13:37 +0100

    tweaks

--- src/components/NotionBlock.tsx
+++ src/components/NotionBlock.tsx
@@ -23,7 +23,7 @@ export default function NotionBlock({ block }: { block: BlockObjectResponse }) {
     case "heading_1":
       if (block.heading_1.rich_text.length === 0) return <br />
       return (
-        <h1 className="mt-8 mb-2 text-3xl font-bold sticky top-[52px] bg-gray-100 z-10">
+        <h1 className="mt-8 mb-2 text-2xl md:text-3xl font-bold sticky top-[52px] bg-gray-100 z-10 min-h-[36px]">
           {block.heading_1.rich_text.map((richText, i) => (
             <NotionRichText key={i} richText={richText} />
           ))}
@@ -43,7 +43,7 @@ export default function NotionBlock({ block }: { block: BlockObjectResponse }) {
       return (
         <h1
           className={cn(
-            "mt-3 mb-1 text-md md:text-lg sticky top-[88px] bg-gray-100 z-10",
+            "mt-3 mb-1 min-h-[50px] md:min-h-0 text-md md:text-lg sticky top-[88px] bg-gray-100 z-10",
             NOTION_COLOR[block.heading_3.color as keyof typeof NOTION_COLOR],
           )}
         >
